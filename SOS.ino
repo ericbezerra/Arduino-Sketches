eb2036ead8f9b31d31fecfa673191410c8871350
Eric Shussan <ericshussan@hotmail.com>
2016-04-30 01:31:43 -0300

    Sketch SOS com buzzer ou led

---
+++ SOS.ino
@@ -0,0 +1,34 @@
+//SOS
+
+int sinal = 12;//sinal de socorro
+int pausaS = 166;
+int pausaO = 500;
+
+void SOS();
+void setup(){
+  pinMode(sinal, OUTPUT);
+}
+
+void loop(){
+  SOS();
+}
+
+void SOS(){
+  for(int i = 0; i < 3; i++){
+    delay(pausaS);
+    digitalWrite(sinal, HIGH);
+    delay(pausaS);
+    digitalWrite(sinal, LOW);
+    
+  }
+  delay(pausaO);
+  for(int i = 0; i < 3; i++){
+    digitalWrite(sinal, HIGH);
+    delay(pausaO);
+    digitalWrite(sinal, LOW);
+    delay(pausaO);
+  }
+}
+
+
+
